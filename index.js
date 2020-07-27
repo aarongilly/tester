4d3fb33c85f66baefb949ac39ff64da7afc94c6b
aarongilly <aarongilly@gmail.com>
2020-07-26 20:56:32 -0500

    tester init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) =>{
+    res.send("Hello World!");
+})
+
+app.get('/echo/:word?',(req, res)=>{
+    if(req.params.word == null) return res.send("Nothing to echo (echo, echo)");
+    console.log(req);
+    res.send("Oh! You said " + req.params.word + "?");
+})
+
+app.listen(8000,()=>{
+    console.log("App is listening on 8000");
+})
